6c1de27e92b13877dff4d67be8de7f906abc90ed
AJ <aletha.e.jones@gmail.com>
2023-11-22 14:32:45 +0000

    minor edits

--- ICSGenerator/Views/MainWindow.axaml.cs
+++ ICSGenerator/Views/MainWindow.axaml.cs
@@ -1,20 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using AJICal;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Platform;
-using Avalonia.Interactivity;
-using Avalonia.Platform;
-using Avalonia.Platform.Storage;
 
 namespace ICSGenerator.Views
 {
     public partial class MainWindow : Window
     {
         public MainWindow()
-        {InitializeComponent();}
+        { InitializeComponent(); }
     }
 }
